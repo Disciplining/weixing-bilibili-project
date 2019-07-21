bf047b50885e16557cac1f91a6ed11b8498e3a6f
unknown <unknown@example.com>
2019-07-21 08:38:48 +0800

    推荐列表加载完毕

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -7,7 +7,8 @@ Page
          */
         data: 
         {
-			videoInfo:{}
+			videoInfo: null,
+            othersList: []
         },
 
         /**
@@ -17,6 +18,8 @@ Page
         {
             let id = options.id;
             this.getCurrentVides(id);
+
+            this.getOthersList(id);
         },
 
         /**
@@ -81,9 +84,12 @@ Page
                     url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videoDetail?id=' + videoId,
                     success: (res)=>
                     {
-                        console.log('请求成功', res);
+                        // console.log('请求成功', res);
 						if(res.data.code === 0)
 						{
+                            // 不知为何，有时code为0但没有视频数据
+                            // console.log('总数据', res)
+                            // console.log('视频数据', res.data.data.videoInfo)
 							that.setData
 							(
 								{
@@ -91,6 +97,38 @@ Page
 								}
 							);
 						}
+                    },
+                    fail: ()=>
+                    {
+                        console.log('请求失败');
+                    }
+                }
+            );
+        },
+        /**
+         * 请求推荐列表数据
+         */
+        getOthersList: function(videoId)
+        {
+            let that = this;
+
+            wx.request
+            (
+                {
+                    url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/othersList?id=' + videoId,
+                    success: (res)=>
+                    {
+                        if (res.data.code === 0)
+                        {
+                            // console.log('赋值的数据', res.data.data.othersList);
+                            that.setData
+                            (
+                                {
+                                    othersList: res.data.data.othersList
+                                }
+                            );
+                        }
+                        // console.log('请求成功', res);
                     },
                     fail: ()=>
                     {
